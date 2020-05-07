e9844a3e7c8938fa8f2315b91ace985d107863db
khDev01 <kasimh2001@outlook.com>
2020-05-07 15:04:44 +0100

    print out text from element

--- webscraper.py
+++ webscraper.py
@@ -5,4 +5,12 @@ url = 'https://atom.io/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-print(content)
+for features in content.find_all('h3'):
+    print(features.text)
+
+# Get all links in within a pages <a> tags
+# for link in content.find_all('a'):
+    # print(link.get('href'))
+# Print all text from a page
+# print(soup.get_text())
+# go to BeautifulSoup doc for more https://www.crummy.com/software/BeautifulSoup/bs4/doc/
