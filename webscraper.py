76c0272e4f68803760bf7d7b83238125d1a96395
khDev01 <kasimh2001@outlook.com>
2020-05-08 14:06:37 +0100

    get item name

--- webscraper.py
+++ webscraper.py
@@ -2,19 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = 'https://atom.io/'
+url = 'https://www.tesco.com/groceries/en-GB/shop/fresh-food/fresh-fruit/all'
 response = requests.get(url, timeout=5)
 # print(response.status_code) # 200 OK page is present
-
 soup = BeautifulSoup(response.content, "html.parser")
 
-featureArr = []
-for features in soup.find_all('h4'):
-    # print(features.text)
-    featureObj = {
-        "feature": features.text
-    }
-    featureArr.append(featureObj)
+# itemsArr = []
+itemheader = soup.find('h3', {"class": "sc-jTzLTM"})
+item = itemheader.findChildren("a", recursive=False)
 
-with open('featureData.json', 'w') as outfile:
-    json.dump(featureArr, outfile)
+for items in item:
+  print(items.text)
+#   itemsObj = {
+#     "items": items.text
+#   }
+#   itemsArr.append(itemsObj)
+#
+# with open('itemsData.json', 'w') as outfile:
+#   json.dump(itemsArr, outfile)
+#
+# li.product-list--list-item
+#   product-tile-wrapper
+#     product-tile
+#     flexi-tile
+#       tile-content
+#         product-details--wrapper
+#           product-details--content
+#             h3.sc-jTzLTM
+#               a.                             productName
+#         product-controls__wrapper
+#           form.
+#             controls
+#               price-details--wrapper
+#                 price-control-wrapper        price
+#                 price-per-quantity-weight    price/weight
